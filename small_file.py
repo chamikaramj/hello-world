0e89483569a2cd99cb9e9e22c1f3f4fbbb83bb86
Chamikara Jayalath <chamikara@google.com>
2016-06-20 13:44:39 -0700

    Adding a small python file.

---
+++ small_file.py
@@ -0,0 +1,17 @@
+"""TODO(chamikara): DO NOT SUBMIT without one-line documentation for small_file.
+
+TODO(chamikara): DO NOT SUBMIT without a detailed description of small_file.
+"""
+
+from google3.pyglib import app
+from google3.pyglib import flags
+
+FLAGS = flags.FLAGS
+
+
+def main(unused_argv):
+  print('some text')
+
+
+if __name__ == '__main__':
+  app.run()
